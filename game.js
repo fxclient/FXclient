8af3fa938a9a4145c02119c4bf0d11b9548637a9
peshomir <peshomir@users.noreply.github.com>
2024-07-02 08:44:42 +0000

    deploy: 1dbcf8af6964d4b8e1c2b168b882d18994a5844d

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ function bE() {
 
 function cc() {
 	this.dG = 1045, this.ct = function() {
-		dA = 2, dC = 23, dB = "27 Jun 2024 [1.96.6]", dD = 0 <= window.location.hostname.toLowerCase().indexOf("territorial.io") || Math.random() >= 0.5, dF = function() {
+		dA = 2, dC = 23, dB = "27 Jun 2024 [1.96.6]", dD = 0 <= window.location.hostname.toLowerCase().indexOf("territorial.io") || Math.random() >= 0.8, dF = function() {
 			try {
 				return window.self !== window.top
 			} catch (dI) {
