175eed0f44b7161ce776990e87dd91d1eb411e8e
peshomir <80340328+peshomir@users.noreply.github.com>
2024-12-26 16:04:22 +0200

    Fix player list not working (Update v0.6.6.11)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-const fx_version = '0.6.6.10'; // FX Client Version
-const fx_update = 'Dec 25'; // FX Client Last Updated
+const fx_version = '0.6.6.11'; // FX Client Version
+const fx_update = 'Dec 26'; // FX Client Last Updated
 
 import settingsManager from './settings.js';
 import { clanFilter, leaderboardFilter } from "./clanFilters.js";
