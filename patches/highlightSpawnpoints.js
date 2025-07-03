05f59c31bda4de1747214d1b7fe8f806d4907946
peshomir <80340328+peshomir@users.noreply.github.com>
2025-07-03 13:53:56 +0300

    Patch for game version 2.08.8

--- patches/highlightSpawnpoints.js
+++ patches/highlightSpawnpoints.js
@@ -1,7 +1,7 @@
 import ModUtils from "../modUtils.js";
 
 // Highlight clan spawnpoints
-export default (/** @type {ModUtils} */ { replaceCode, waitForMinification, replaceRawCode }) => {
+export default (/** @type {ModUtils} */ { replaceCode }) => {
     // when rendered on game start
     replaceCode(
         `var x = (d.minX[i] + d.maxX[i] + 1 - hQ - 2) >> 1;
@@ -14,13 +14,22 @@ export default (/** @type {ModUtils} */ { replaceCode, waitForMinification, repl
         if (highlight) ctx.drawImage(a1e[game.gIsTeamGame ? ((game.gGameType === 9 && aD.a[i] === 5) ? 3 : t.ids[i]) : (i < game.gHumans ? 1 : 0)], x, y, hQ, hQ);
         else ctx.drawImage(a1e[game.gIsTeamGame ? ((game.gGameType === 9 && aD.a[i] === 5) ? 3 : t.ids[i]) : (i < game.gHumans ? 1 : 0)], x, y);`,
     )
-    waitForMinification(() => {
-        // when rendered during spawn selection
-        replaceRawCode("function(a6,es,bK,hP,hQ,hR,ov){0===dV.eg[a6]||0===dV.ev[a6]||(hQ=ay.ak*((dV.nu[a6]+dV.nw[a6]+1)/2-bK)/(hQ-bK)-.5*es,bK=ay.al*((dV.nv[a6]+dV.nx[a6]+1)/2-hP)/(hR-hP)-.5*es,hQ>ay.ak)||bK>ay.al||hQ<-es||bK<-es||(bI.setTransform(cz,0,0,cz,hQ,bK),",
-            `function(a6,es,bK,hP,hQ,hR,ov){
-            var highlight = __fx.settings.highlightClanSpawns && __fx.clanFilter.inOwnClan[a6];
-            if (highlight) es *= 2;
-            0===dV.eg[a6]||0===dV.ev[a6]||(hQ=ay.ak*((dV.nu[a6]+dV.nw[a6]+1)/2-bK)/(hQ-bK)-.5*es,bK=ay.al*((dV.nv[a6]+dV.nx[a6]+1)/2-hP)/(hR-hP)-.5*es,hQ>ay.ak)||bK>ay.al||hQ<-es||bK<-es||(bI.setTransform(highlight?cz*2:cz,0,0,highlight?cz*2:cz,hQ,bK),`
-        )
-    })
+
+    // when rendered during spawn selection
+    replaceCode(
+        `var x = f.w * ((playerData.minX[i] + playerData.maxX[i] + 1) / 2 - gE) / (li - gE) - 0.5 * j7;
+		var y = f.h * ((playerData.minY[i] + playerData.maxY[i] + 1) / 2 - ex) / (lj - ex) - 0.5 * j7;
+		if (x > f.w || y > f.h || x < -j7 || y < -j7) { return; }
+		mainCanvas.setTransform(bg, 0, 0, bg, x, y);
+		mainCanvas.drawImage(vL[game.gIsTeamGame ? TeamManager.ids[i] : 1], 0, 0);`,
+
+        `var highlight = __fx.settings.highlightClanSpawns && __fx.clanFilter.inOwnClan[i];
+        if (highlight) j7 *= 2;
+		var x = f.w * ((playerData.minX[i] + playerData.maxX[i] + 1) / 2 - gE) / (li - gE) - 0.5 * j7;
+		var y = f.h * ((playerData.minY[i] + playerData.maxY[i] + 1) / 2 - ex) / (lj - ex) - 0.5 * j7;
+		if (x > f.w || y > f.h || x < -j7 || y < -j7) { return; }
+		if (highlight) mainCanvas.setTransform(bg*2, 0, 0, bg*2, x, y);
+		else mainCanvas.setTransform(bg, 0, 0, bg, x, y);
+		mainCanvas.drawImage(vL[game.gIsTeamGame ? TeamManager.ids[i] : 1], 0, 0);`
+    )
 }
